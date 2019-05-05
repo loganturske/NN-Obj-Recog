8c88532173ca424a1f8d3038df9bbc9dec063c8e
jpdixon802 <49133498+jpdixon802@users.noreply.github.com>
2019-05-05 10:34:29 -0400

    Seed random number generator
    I added a seed to the random number generator to insure when were testing we will get repeatable results

--- Network.py
+++ Network.py
@@ -3,10 +3,11 @@ import numpy as np
 
 class Network(object):
 
-	def __init__(self, sizes):
+	def __init__(self, sizes, seed=0):
 		self.num_layers = len(sizes)
 		self.sizes = sizes
 		# Random weights based on Gaussian distribution
+		np.random.seed(seed=seed)
 		self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
 		self.weights = [np.random.randn(y, x)
 		for x, y in zip(sizes[:-1], sizes[1:])]
